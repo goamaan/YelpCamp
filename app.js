061bc3a209754b175daff5b603b4a0535089e644
Amaan18 <amaangokak18@gmail.com>
2020-05-01 16:30:50 -0700

    added mongoose

--- app.js
+++ app.js
@@ -1,90 +1,86 @@
-const app = require("express")();
-const bodyParser = require("body-parser");
+const app = require("express")(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
-let campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80",
-  },
-  {
-    name: "Kyrat",
-    image:
-      "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80",
-  },
-  {
-    name: "Kyrat",
-    image:
-      "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80",
-  },
-  {
-    name: "Kyrat",
-    image:
-      "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80",
-  },
-  {
-    name: "Kyrat",
-    image:
-      "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-  },
-];
+mongoose.connect("mongodb://localhost/yelp_camp", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+//SCHEMA SETUP
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+//Landing page
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
+//Index
 app.get("/campgrounds", (req, res) => {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  Campground.find({}, (err, campgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { campgrounds: campgrounds });
+    }
+  });
+});
+
+// Campground.create(
+//   {
+//     name: "Granite Hill",
+//     image:
+//       "https://pixabay.com/get/57e1dd4a4350a514f1dc84609620367d1c3ed9e04e5074417d2d7fdd964fc7_340.jpg",
+//     description:
+//       "Starry night sky aoifsnesof asof aois fiose fo esaiofniosnfajsfoias fioa o",
+//   },
+//   (err, campground) => {
+//     if (err) {
+//       console.log(error);
+//     } else {
+//       console.log("Created new campground");
+//       console.log(campground);
+//     }
+//   }
+// );
+
+app.post("/campgrounds", (req, res) => {
+  const newCampground = {
+    name: req.body.name,
+    image: req.body.image,
+    description: req.body.description,
+  };
+
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get("/campgrounds/new", (req, res) => {
   res.render("new");
 });
 
-app.post("/campgrounds", (req, res) => {
-  const newCampground = {
-    name: req.body.name,
-    image: req.body.image,
-  };
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+app.get("/campgrounds/:id", (req, res) => {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", { campground: foundCampground });
+    }
+  });
 });
+
 app.listen(3000, () => console.log("Listening on port 3000"));
