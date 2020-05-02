f280d0998f9a8eaac9d4502bc96a791506084201
Amaan18 <amaangokak18@gmail.com>
2020-05-01 18:08:47 -0700

    added comment routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = require("express")(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
   Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
   seedDB = require("./seeds");
 
 seedDB();
@@ -26,7 +27,7 @@ app.get("/campgrounds", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.render("index", { campgrounds: campgrounds });
+      res.render("campgrounds/index", { campgrounds: campgrounds });
     }
   });
 });
@@ -49,7 +50,7 @@ app.post("/campgrounds", (req, res) => {
 
 //NEW ROUTE FORM
 app.get("/campgrounds/new", (req, res) => {
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
 //SHOW ROUTE
@@ -60,9 +61,44 @@ app.get("/campgrounds/:id", (req, res) => {
       if (err) {
         console.log(err);
       } else {
-        res.render("show", { campground: foundCampground });
+        res.render("campgrounds/show", { campground: foundCampground });
       }
     });
+});
+
+//========================
+//COMMENTS ROUTES
+//========================
+
+//NEW COMMENT FORM
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", { campground: campground });
+    }
+  });
+});
+
+//COMMENT POST ROUTE
+app.post("/campgrounds/:id/comments", (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect(`/campgrounds/${campground._id}`);
+        }
+      });
+    }
+  });
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
