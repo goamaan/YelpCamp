15724ff8745e7f06af5f4c6f156ad334aea39db2
Amaan18 <amaangokak18@gmail.com>
2020-05-01 17:28:00 -0700

    added comments in db

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const app = require("express")(),
   bodyParser = require("body-parser"),
-  mongoose = require("mongoose");
+  mongoose = require("mongoose"),
+  Campground = require("./models/campground"),
+  seedDB = require("./seeds");
 
+seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
 
 //SCHEMA SETUP
-const campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String,
-});
-
-const Campground = mongoose.model("Campground", campgroundSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -35,24 +31,6 @@ app.get("/campgrounds", (req, res) => {
   });
 });
 
-// Campground.create(
-//   {
-//     name: "Granite Hill",
-//     image:
-//       "https://pixabay.com/get/57e1dd4a4350a514f1dc84609620367d1c3ed9e04e5074417d2d7fdd964fc7_340.jpg",
-//     description:
-//       "Starry night sky aoifsnesof asof aois fiose fo esaiofniosnfajsfoias fioa o",
-//   },
-//   (err, campground) => {
-//     if (err) {
-//       console.log(error);
-//     } else {
-//       console.log("Created new campground");
-//       console.log(campground);
-//     }
-//   }
-// );
-
 app.post("/campgrounds", (req, res) => {
   const newCampground = {
     name: req.body.name,
@@ -69,18 +47,22 @@ app.post("/campgrounds", (req, res) => {
   });
 });
 
+//NEW ROUTE FORM
 app.get("/campgrounds/new", (req, res) => {
   res.render("new");
 });
 
+//SHOW ROUTE
 app.get("/campgrounds/:id", (req, res) => {
-  Campground.findById(req.params.id, (err, foundCampground) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("show", { campground: foundCampground });
-    }
-  });
+  Campground.findById(req.params.id)
+    .populate("comments")
+    .exec((err, foundCampground) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("show", { campground: foundCampground });
+      }
+    });
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
