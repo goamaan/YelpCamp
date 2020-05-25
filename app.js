860dbbda8b726e173b2e262739deb19c65a8b42b
Amaan18 <amaangokak18@gmail.com>
2020-05-24 23:54:19 -0700

    quotes lol

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-require("dotenv").config();
-const express = require("express"),
-  bodyParser = require("body-parser"),
-  mongoose = require("mongoose"),
-  passport = require("passport"),
-  LocalStrategy = require("passport-local"),
-  methodOverride = require("method-override"),
-  flash = require("connect-flash"),
-  Campground = require("./models/campground"),
-  Comment = require("./models/comment"),
-  User = require("./models/user"),
-  seedDB = require("./seeds"),
+require('dotenv').config();
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local'),
+  methodOverride = require('method-override'),
+  flash = require('connect-flash'),
+  Campground = require('./models/campground'),
+  Comment = require('./models/comment'),
+  User = require('./models/user'),
+  seedDB = require('./seeds'),
   app = express();
 
-const commentRoutes = require("./routes/comments"),
-  campgroundRoutes = require("./routes/campgrounds"),
-  indexRoutes = require("./routes/index");
+const commentRoutes = require('./routes/comments'),
+  campgroundRoutes = require('./routes/campgrounds'),
+  indexRoutes = require('./routes/index');
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.DATABASEURL || "mongodb://localhost/yelp_camp", {
+mongoose.connect(process.env.DATABASEURL || 'mongodb://localhost/yelp_camp', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false,
@@ -26,8 +26,8 @@ mongoose.connect(process.env.DATABASEURL || "mongodb://localhost/yelp_camp", {
 
 //PASSPORT CONFIGURATION
 app.use(
-  require("express-session")({
-    secret: "First fullstack project!",
+  require('express-session')({
+    secret: 'First fullstack project!',
     resave: false,
     saveUninitialized: false,
   })
@@ -36,26 +36,26 @@ app.use(
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.locals.moment = require("moment");
+app.locals.moment = require('moment');
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
-  res.locals.error = req.flash("error");
-  res.locals.success = req.flash("success");
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
   next();
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 // seedDB();
 
 app.use(indexRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(port, () => console.log("Server started!"));
+app.listen(port, () => console.log('Server started!'));
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,56 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
-const User = require("../models/user");
+const passport = require('passport');
+const User = require('../models/user');
 
 //Landing page
-router.get("/", (req, res) => {
-  res.render("landing");
+router.get('/', (req, res) => {
+  res.render('landing');
 });
 
 //GET REGISTER FORM
-router.get("/register", (req, res) => {
-  res.render("register", { page: "register" });
+router.get('/register', (req, res) => {
+  res.render('register', { page: 'register' });
 });
 
 //SIGNUP POST ROUTE
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   const newUser = new User({ username: req.body.username });
-  if (req.body.adminCode === "admin123") {
+  if (req.body.adminCode === 'admin123') {
     newUser.isAdmin = true;
   }
   User.register(newUser, req.body.password, (err, user) => {
     if (err) {
-      req.flash("error", err.message);
-      return res.redirect("register");
+      req.flash('error', err.message);
+      return res.redirect('register');
     }
-    passport.authenticate("local")(req, res, () => {
-      req.flash("success", `Welcome to YelpCamp ${user.username}`);
-      res.redirect("/campgrounds");
+    passport.authenticate('local')(req, res, () => {
+      req.flash('success', `Welcome to YelpCamp ${user.username}`);
+      res.redirect('/campgrounds');
     });
   });
 });
 
 //GET LOGIN FORM
-router.get("/login", (req, res) => {
-  res.render("login", { page: "login" });
+router.get('/login', (req, res) => {
+  res.render('login', { page: 'login' });
 });
 
 //LOG IN POST ROUTE
-router.post("/login", function (req, res, next) {
-  passport.authenticate("local", {
-    successRedirect: "/campgrounds",
-    failureRedirect: "/login",
+router.post('/login', function (req, res, next) {
+  passport.authenticate('local', {
+    successRedirect: '/campgrounds',
+    failureRedirect: '/login',
     failureFlash: true,
-    successFlash: "Welcome to YelpCamp, " + req.body.username + "!",
+    successFlash: 'Welcome to YelpCamp, ' + req.body.username + '!',
   })(req, res);
 });
 
 //LOG OUT ROUTE
-router.get("/logout", (req, res) => {
+router.get('/logout', (req, res) => {
   req.logout();
-  req.flash("success", "Logged out!");
-  res.redirect("/campgrounds");
+  req.flash('success', 'Logged out!');
+  res.redirect('/campgrounds');
 });
 
 module.exports = router;
